78aaa0ffc78f9b08c1b27efcf7c0a75a4c152c42
Bryan Bush <bryan@dialexa.com>
2020-06-07 20:51:10 -0500

    Handle IDs.

--- FEHub.Entity/FehContextFactory.cs
+++ FEHub.Entity/FehContextFactory.cs
@@ -8,15 +8,12 @@ using System.Configuration;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Logging;
 
 namespace FEHub.Entity
 {
     public class FehContextFactory : IDesignTimeDbContextFactory<FehContext>
     {
         #region Fields
-        private static readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(builder => { builder.AddDebug(); });
-
         private static readonly string _connectionString = ConfigurationManager.ConnectionStrings["FEHub"]?.ConnectionString 
             //?? "Data Source=localhost;Initial Catalog=FEHub;Integrated Security=true";
             ?? @"Data Source=C:\Source\FEHub\fehub\fehub-api\FEHub.sqlite3;";
